1b6a6ec24c6d6f33e1553ead689d8944b47b42cf
Tebalelo Sekhula <tebalelo.sekhula@dadevs.co.za>
2022-04-25 20:55:16 +0200

    make it better

--- Routes/assignedSubject.js
+++ Routes/assignedSubject.js
@@ -6,6 +6,6 @@ router.get('/viewMySubjects/:id', subjectCrud.viewMySubjects);
 router.delete('/removeSubject/:id', subjectCrud.removeSubject);
 router.post('/addMySubject/', subjectCrud.addSubject);
 router.get('/getTeachers/:id', subjectCrud.getTeachers);
-
+//
 
 module.exports = router;
